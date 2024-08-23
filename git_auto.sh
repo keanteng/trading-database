3ca2c71d61482ab2664ea63b73a9b1fd6b390a29
keanteng <u2004763@siswa.um.edu.my>
2024-08-23 23:15:02 +0800

    add shell auto commit

--- git_auto.sh
+++ git_auto.sh
@@ -9,8 +9,15 @@ next_commit_count=$((commit_count + 1))
 # Add all changes
 git add .
 
-# Commit with the incremented commit count in the message
-git commit -m "auto commit #$next_commit_count"
+# Check if a custom message is provided
+if [ -z "$1" ]; then
+  commit_message="auto commit #$next_commit_count"
+else
+  commit_message="$1"
+fi
+
+# Commit with the message
+git commit -m "$commit_message"
 
 # Push the changes
 git push
